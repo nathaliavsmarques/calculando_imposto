432018a130a675a7f666ae09e8ef81e665dfa39b
nathaliavsmarques <nathaliavsmarques@outlook.com>
2023-01-18 16:18:05 -0300

    arquivos adicionados

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let productPrice = document.querySelector('#boxprice');
+let btn = document.querySelector('#btn');
+let text = document.querySelector('#text');
+const tax = 0.6;
+
+btn.addEventListener('click', function calcular() {
+    let taxOnProduct = parseFloat(productPrice.value) * tax;
+    let totalPrice = parseFloat(productPrice.value) + (parseFloat(productPrice.value) * tax);
+
+    if (productPrice.value <= 0) {
+        window.alert('ERRO! Por favor preencha com um valor válido!');
+    } else if (productPrice.value <= 150) {
+        text.innerHTML = `Seu produto não será taxado.`;
+        text.style.backgroundColor = '#11F05F';
+        text.style.textAlign = 'center';
+    } else {
+        text.innerHTML = `Será cobrado ${taxOnProduct.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} reais de imposto. Ao total você irá   
+        pagar ${totalPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} reais.`;
+        text.style.backgroundColor = '#FF5533';
+        text.style.textAlign = 'center';
+    }
+
+
+});
+
