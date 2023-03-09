59391fa3e3169af7c87bb542e9e9373da75adf9e
nathaliavsmarques <nathaliavsmarques@outlook.com>
2023-03-09 17:07:07 -0300

    mudanças no estilo e correções no código

--- js/script.js
+++ js/script.js
@@ -6,20 +6,22 @@ const tax = 0.6;
 btn.addEventListener('click', function() {
     let taxOnProduct = parseFloat(productPrice.value) * tax;
     let totalPrice = parseFloat(productPrice.value) + (parseFloat(productPrice.value) * tax);
-
+    
+    
     if (productPrice.value <= 0) {
-        window.alert('Por favor preencha com um valor válido!');
+        window.alert('Por favor, preencha o campo abaixo com um valor válido!');
     } else if (productPrice.value <= 150) {
         text.innerHTML = `Seu produto não será taxado.`;
         text.style.backgroundColor = '#11F05F';
         text.style.textAlign = 'center';
     } else {
-        text.innerHTML = `Será cobrado ${taxOnProduct.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} reais de imposto. Ao total você irá   
-        pagar ${totalPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} reais.`;
+        text.innerHTML = `Será cobrado ${taxOnProduct.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} reais de imposto. Ao total você irá pagar ${totalPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} reais.`;
         text.style.backgroundColor = '#FF5533';
         text.style.textAlign = 'center';
     }
-
+    
+    productPrice.value = ''; 
+    productPrice.focus();
 
 });
 
